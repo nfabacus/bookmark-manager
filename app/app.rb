fa0e6bbd4e90d3cf8f9c943fb3bdb539aeb98054
noby <n.fujioka@abacusmaths.info>
2016-07-26 17:27:22 +0100

    ex11: Creating links

--- app/app.rb
+++ app/app.rb
@@ -2,15 +2,22 @@ require 'sinatra/base'
 require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+  get '/' do
+    redirect '/links'
+  end
+  
   get '/links' do
     @links = Link.all
     erb :'links/index'
   end
 
-  post '/links/new' do
+  get '/links/new' do
     erb :'links/new'
-    #Link.create(url: params['url'], title: params['title'])
-    #erb :'links/index'
+  end
+
+  post '/temporaryLink' do
+      Link.create(url: params['url'], title: params['title'])
+      redirect '/links'
   end
 
   # start the server if ruby file executed directly
--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,9 +1,12 @@
 feature 'create links' do
   scenario 'adding a new link to bookmark manager' do
     visit '/links/new'
-    fill_in 'url', with: 'http://www.google.com'
-    fill_in 'title', with: 'Google'
+    fill_in :url, with: 'http://www.google.com'
+    fill_in :title, with: 'Google'
     click_button 'Add'
-    expect(page).to have_content('Google')
+
+    within 'ul#links' do
+      expect(page).to have_content('Google')
+    end
   end
 end
