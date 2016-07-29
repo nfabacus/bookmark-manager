a288af8d90231b3ada542a400b5bd16f1582c01d
acsauk <acsauk@gmail.com>
2016-07-29 12:16:06 +0100

    completes exercise #22 - adds functionailty to check for duplicate emails and refactors error message display using errors.full_messages

--- app/app.rb
+++ app/app.rb
@@ -51,17 +51,27 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/newuser' do
-    user = User.new(username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    user = User.new(username: params[:username],
+                    email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation])
 
     if user.save
       session[:user_id] = user.id
       redirect '/links'
-    elsif user.password != user.password_confirmation
-      flash.now[:notice] = "Password and confirmation password do not match."
+    else
+      flash.now[:errors] = user.errors.full_messages
       erb :'links/userform'
-    elsif user.email.empty?
-      flash.now[:notice] = "Please enter your email address."
-      erb :'links/userform'
+    # elsif user.password != user.password_confirmation
+    #   flash.now[:notice] = "Password and confirmation password do not match."
+    #   erb :'links/userform'
+    # elsif user.email.empty?
+    #   flash.now[:notice] = "Please enter your email address."
+    #   erb :'links/userform'
+    # elsif !User.all(email: user.email).empty?
+    #   flash.now[:notice] = "Email address already exists - please use unique email address"
+    #   erb :'links/userform'
+    # end
     end
   end
 
--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -1,45 +1,36 @@
 feature 'create user account' do
   scenario 'I can sign up as a new user' do
-    visit '/sign-up'
-    fill_in 'username', with: 'Fran'
-    fill_in 'email', with: 'fran@makers.com'
-    fill_in 'password', with: 'guitar1234'
-    fill_in 'password_confirmation', with: 'guitar1234'
+    sign_up
     expect{ click_button 'signup' }.to change(User, :count).by(1)
-    expect(page).to have_content('Welcome, Fran')
-    expect(User.first.email).to eq('fran@makers.com')
+    expect(page).to have_content('Welcome, example')
+    expect(User.first.email).to eq('example@gmail.com')
   end
 
   scenario 'no new user is created with mismatching passwords' do
-    visit '/sign-up'
-    fill_in 'username', with: 'Fran'
-    fill_in 'email', with: 'fran@makers.com'
-    fill_in 'password', with: 'guitar1234'
-    fill_in 'password_confirmation', with: 'piano1234'
+    sign_up(password_confirmation: "1234")
     expect { click_button 'signup' }.not_to change(User, :count)
     expect(current_path).to eq('/newuser')
-    expect(page).to have_content("Password and confirmation password do not match")
+    expect(page).to have_content("Password does not match the confirmation")
   end
 
   scenario 'I can not sign up without entering an email' do
-    visit '/sign-up'
-    fill_in 'username', with: 'Fran'
-    fill_in 'email', with: ''
-    fill_in 'password', with: 'guitar1234'
-    fill_in 'password_confirmation', with: 'guitar1234'
+    sign_up(email: "")
     expect { click_button 'signup' }.not_to change(User, :count)
     expect(current_path).to eq('/newuser')
-    expect(page).to have_content("Please enter your email address.")
+    expect(page).to have_content("Email must not be blank")
   end
 
   scenario 'I can not sign up using an invalid email format' do
-    visit '/sign-up'
-    fill_in 'username', with: 'Fran'
-    fill_in 'email', with: 'text'
-    fill_in 'password', with: 'guitar1234'
-    fill_in 'password_confirmation', with: 'guitar1234'
+    sign_up(email: "example@gmail")
     expect { click_button 'signup' }.not_to change(User, :count)
-    # expect(current_path).to eq('/sign-up')
-    # expect(page).to have_content("Invalid email format")
+    expect(page).to have_content('Email has an invalid format')
+  end
+
+  scenario 'I can not sign up using a duplicate email address' do
+     sign_up
+     click_button 'signup'
+     sign_up
+     expect { click_button 'signup' }.not_to change(User, :count)
+     expect(page).to have_content("Email is already taken")
   end
 end
